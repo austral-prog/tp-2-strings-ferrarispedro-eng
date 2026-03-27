705700641ef67836eb95ac65fbf2e8c4e96f3b7b
ferrarispedro-eng <ferrarispedro@gmail.com>
2026-03-27 11:48:27 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -2,4 +2,32 @@ def change():
     """Lee un gasto y el dinero recibido, calcula el vuelto
     y lo separa en pesos (parte entera) y centavos.
     """
-    pass
+    gasto = float(input("Ingresar gasto: "))
+
+    dinero_recibido = int(input("Ingresa el dinero recibido: "))
+
+    print("Ingresar gasto:"
+          "")
+    print(gasto)
+
+    print("Dinero recibido"
+          "")
+    print(dinero_recibido)
+
+    print("")
+
+    print("Vuelto")
+
+    print("")
+    print("Pesos:")
+
+    vuelto = dinero_recibido - gasto
+
+    pesos = int(vuelto)
+    centavos = round((vuelto - pesos) * 100)
+
+    print(pesos)
+    print("Centavos:")
+    print(centavos)
+
+#change()
